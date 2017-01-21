77b325d85d918a17cb6408b21dea973d9d0a67ff
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-01-20 19:11:33 -0800

    Format tweet messages

--- backend/tweet-bot-service.js
+++ backend/tweet-bot-service.js
@@ -8,10 +8,18 @@ const env = require('./env');
 rule.hour = 5;
 
 module.exports = db => {
-  schedule.scheduleJob(rule, function() {
+  schedule.scheduleJob(rule, async function() {
     // Need to read the DB for today's day, if any events then tweet
     // them
-    tweet('Hello World tweeted automated');
+    let events =
+	await db.all(`
+select * from event 
+    where start = (select max(start) from event WHERE start < DATE('now'))
+`);
+    Promise.all(
+      events.map(({title, description, url}) =>
+		 tweet({title, description, url }, false))
+    );
   });
   
 };
--- backend/tweet-events.js
+++ backend/tweet-events.js
@@ -6,4 +6,22 @@ const env = require('./env');
 const client = env.production ? new Twitter(env.twitter) : null;
 
 module.exports =
-  async (msg) => client.post('statuses/update', {status: msg.slice(0, 140)});
+  async ({title, description, url}, new_event=true) =>
+  client.post('statuses/update', {
+    status: new_event ?
+      (() => {
+	const s =
+	      `
+New event on silicondzor.com ${title.slice(0, 20)}, ${description.slice(0, 25)}
+`;
+	return `${s}${url.slice(0, 140 - s.length)}`;
+      })() :
+      (() => {
+	const s =
+	      `
+Today's tech in Armenia: ${title.slice(0, 20)}, ${description.slice(0, 25)}...
+`;
+	return `${s}${url.slice(0, 140 - s.length)}`;
+      })()
+
+  });
