792d8c6e3cfb52b32fcf6de5b479e89f2268d64f
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-01-20 18:24:29 -0800

    Fix key name typo

--- backend/tweet-events.js
+++ backend/tweet-events.js
@@ -3,7 +3,7 @@
 const Twitter = require('twitter');
 const env = require('./env');
 
-const client = env.prod ? new Twitter(env.twitter) : null;
+const client = env.production ? new Twitter(env.twitter) : null;
 
 module.exports =
   async (msg) => client.post('statuses/update', {status: msg.slice(0, 140)});
