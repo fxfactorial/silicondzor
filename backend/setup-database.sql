52fcd61841212a25fcfbb39e9c160327ce5f6961
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-02-09 16:48:10 -0800

    Add post table

--- backend/setup-database.sql
+++ backend/setup-database.sql
@@ -4,6 +4,8 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE account (
     id INTEGER PRIMARY KEY autoincrement,
+
+    username TEXT NOT NULL,
     email TEXT NOT NULL,
     -- Holds a bcrypted hash
     hashed_password TEXT NOT NULL,
@@ -23,7 +25,20 @@ CREATE TABLE event (
     UNIQUE (id)
 );
 
-CREATE TABLE comments (
+CREATE TABLE post (
+   creator INTEGER PRIMARY KEY NOT NULL,
+   FOREIGN KEY (creator) REFERENCES account(ID)
+
+   creation_time INTEGER NOT NULL,
+   title TEXT NOT NULL,
+   content TEXT,
+   web_link TEXT,
+   upvotes INTEGER NOT NULL DEFAULT 1,
+   downvotes INTEGER NOT NULL DEFAULT 0,
+   comment_count INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE comment (
    creator INTEGER PRIMARY KEY NOT NULL,
 
    --Make sure this goes through XSS Filter first before entering the
@@ -33,6 +48,6 @@ CREATE TABLE comments (
    upvotes INTEGER NOT NULL DEFAULT 0,
    downvotes INTEGER NOT NULL DEFAULT 0,
 
-   FOREIGN KEY (parent)  REFERENCES comments(id),
+   FOREIGN KEY (parent)  REFERENCES comment(id),
    FOREIGN KEY (creator) REFERENCES account(ID)
 );
