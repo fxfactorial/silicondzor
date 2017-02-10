72bcf2139de1690b959d17b254ad322df52787ef
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-02-09 19:59:35 -0800

    Optimize webpack for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -16,9 +16,27 @@ module.exports = {
     extensions: [' ', '.js', '.jsx']
   },
   plugins: [
+    new webpack.LoaderOptionsPlugin({minimize:true}),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false, // Suppress uglification warnings
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false
+      },
+      exclude: [/\.min\.js$/gi] // skip pre-minified libs
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+    new webpack.optimize.AggressiveMergingPlugin,
+    new webpack.optimize.OccurrenceOrderPlugin,
     new webpack.DefinePlugin({
       'process.env': {
-	'NODE_ENV': JSON.stringify('production')
+	      'NODE_ENV': JSON.stringify('production')
       }
     })
   ],
@@ -31,7 +49,7 @@ module.exports = {
         query: {
           // Here you can put plugins, like plugins:['transform-runtime']
           plugins:['transform-class-properties'],
-          presets: ['es2015', 'react', 'stage-3']
+          presets: [['es2015', {modules: false}], 'react', 'stage-3']
         }
       }]
   }
