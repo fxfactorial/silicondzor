7de136eeb06ae8eacf130841eb4e073fc55cb194
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-02-27 04:31:02 -0800

    Whitespace

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,10 +1,11 @@
 // import {Application, TechCalendar} from '../lib/silicondzor';
 import News from '../lib/dzor-news';
-import JobsBoard from '../lib/jobs-board';
 import routes from '../lib/Routes';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
+
+import { Router, Route, Link, browserHistory, IndexRoute }
+from 'react-router';
 // Solely because of Safari's fetch sucking
 import 'whatwg-fetch';
 
