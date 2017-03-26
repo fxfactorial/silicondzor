44b0c80decb98f0f1a6350767fa36c93fe46984e
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-03-27 00:57:04 +0400

    add style to login

--- lib/login.jsx
+++ lib/login.jsx
@@ -3,9 +3,48 @@ import React, { Component } from 'react';
 export default
 class SDLogin extends Component {
   render () {
+    const titleStyle = {
+      fontSize: 40,
+      marginBottom: 20,
+    };
+    const buttonStyle = {
+      fontSize: 10,
+      padding: 4,
+      borderRadius: 3,
+      marginTop: 10
+    };
     return (
       <div>
-        Some kind of Login screen
+        <div style={titleStyle}>
+          Login
+        </div>
+        <div>
+          username:
+          <input />
+        </div>
+        <div>
+          password:
+          <input />
+        </div>
+        <button style={buttonStyle}>
+          login
+        </button>
+        
+        <div style={titleStyle}>
+          Create account
+        </div>
+        <div>
+          username:
+          <input />
+        </div>
+        <div>
+          password:
+          <input />
+        </div>
+        <button style={buttonStyle}>
+          create account
+        </button>
+        
       </div>
     );
   }
