16a14f2654e0a7d83725b8fc2cc3cc03d6ab5a18
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-03-27 23:22:16 +0400

    adding submit connection

--- lib/submit-news.jsx
+++ lib/submit-news.jsx
@@ -1,10 +1,73 @@
 import React, { Component } from 'react';
-
+import {request_opts} from './utility';
 export default class SDSubmitNews extends Component {
-  render() {
+  constructor(){
+    super();
+    this.state = {
+      title: '',
+      url: '',
+      content: ''
+    }
+    this.titleChange = this.titleChange.bind(this);
+    this.urlChange = this.urlChange.bind(this);
+    this.contentChange = this.contentChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+  async submit(){
+    const {title, url, content} = this.state;
+    const send_to_server = request_opts(JSON.stringify({title, web_url: url, content}));
+    const answer = await fetch(`http://localhost:9090/submit-post`, send_to_server);
+    const answer_json = await answer.json();
+    console.log(answer_json);
+  }
+  titleChange(e){
+    const title = e.currentTarget.value;
+    this.setState({title});
+  }
+  urlChange(e){
+    const url = e.currentTarget.value;
+    this.setState({url});
+  }
+  contentChange(e){
+    const content = e.currentTarget.value;
+    this.setState({content});
+  }
+  render () {
+    const titleStyle = {
+      fontSize: 40,
+      marginBottom: 10,
+    };
+    const buttonStyle = {
+      fontSize: 15,
+      padding: 4,
+      borderRadius: 3,
+      marginTop: 10
+    };
     return (
       <div>
-        Need to submit a news story, this will be some form
+        <div style={titleStyle}>
+          Submit post form
+        </div>
+        <div style={{fontSize: 20}}>
+          title:
+          <input onChange={this.titleChange}/>
+        </div>
+        <div style={{fontSize: 20}}>
+          url:
+          <input onChange={this.urlChange}/>
+        </div>
+        <div style={{fontSize: 20}}>
+          OR
+        </div>
+        <div>
+          <div style={{fontSize: 20}}>
+            text:
+          </div>
+          <textarea onChange={this.contentChange} style={{width: 500, height: 200}} />
+        </div>
+        <button style={buttonStyle} onClick={this.submit}>
+          submit
+        </button>
       </div>
     );
   }
