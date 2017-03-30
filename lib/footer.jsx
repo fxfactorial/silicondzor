c0ac79a15361bc4cb7320ef702f976d01b89ada1
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-03-30 05:54:09 -0700

    Expand out footer

--- lib/footer.jsx
+++ lib/footer.jsx
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import colors from './colors';
 
 const bottom_s = {
-  marginTop:'20px',
-  color:'white',
+  paddingTop:'20px',
   width:`100%`,
-  bottom:0,
-  marginBottom:'10px',
-  backgroundColor:colors.site_colors.banner
+  paddingBottom:'10px'
+};
+
+const bar_s = {
+  backgroundColor:colors.site_colors.banner,
+  height:'5px'
+};
+
+const footer_row = {
+  marginTop:'10px',
+  display:'flex',
+  justifyContent:'center'
+};
+
+const grouping_s = {
+  display:'inline-flex',
+  width:'220px',
+  justifyContent:'space-between'
 };
 
 export default class Footer extends Component {
   render () {
     return (
       <footer style={bottom_s}>
-	      Should be bottom footer
+        <hr style={bar_s}/>
+        <section style={footer_row}>
+
+          <section style={grouping_s}>
+            <Link to={'/guidelines'}>Guidelines</Link> |
+            <Link to={'/faq'}>FAQ</Link> |
+            <a href={'https://github.com/fxfactorial/silicondzor'}>Source code</a>
+          </section>
+
+        </section>
+
+        <section style={footer_row}>
+          <p style={{marginRight:'5px'}}>Search:</p>
+          <input type={'search'}/>
+        </section>
       </footer>
     );
   }
