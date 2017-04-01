aa4e11fce84109a65e774a15e63726d943db2a23
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-04-01 08:38:37 -0700

    Expand out job board

--- lib/job-board.jsx
+++ lib/job-board.jsx
@@ -3,13 +3,14 @@ import colors from './colors';
 import { ital } from './utility';
 
 const job_card_s = {
-  marginTop:'10px',
-  padding:'10px',
+  margin:'10px',
+  borderRadius:'5px',
+  padding:'5px',
   backgroundColor:colors.site_colors.cards
 };
 
 const JobCard = (
-  {contact_info, employer, downvote_handler,
+  {contact_info, employer, downvote_handler, idx,
    upvotes, downvotes, upvote_handler, job_title,
    job_descr, location, salary}) => (
     <section style={job_card_s} key={job_descr}>
@@ -17,14 +18,18 @@ const JobCard = (
         {upvotes}
         <i onClick={upvote_handler}
            className={'material-icons'}>arrow_upward</i>
-        {downvotes}
+        <span style={{marginLeft:'5px'}}>{downvotes}</span>
         <i onClick={downvote_handler}
            className={'material-icons'}>arrow_downward</i>
         {ital('Posted by: ')} {employer}, {ital('location: ')} {location}
       </p>
-      <p style={{paddingLeft:'15px'}}>
-        {job_title}
-      </p>
+      <hr/>
+      <details open={false} style={{marginTop:'10px'}}>
+        <summary>
+          <span style={{fontSize:'1.5em'}}>{job_title}</span>
+        </summary>
+        {job_descr}
+      </details>
     </section>
   );
 
@@ -43,10 +48,11 @@ export default class SDJobs extends Component {
     const { all_jobs } = this.props;
     console.log(this.props);
     const postings =
-          all_jobs.map(props => (
+          all_jobs.map((props, idx) => (
             <JobCard upvote_handler={this.upvote}
                      downvote_handler={this.downvote}
                      {...props}
+                     idx={idx + 1}
                      key={props.job_descr}
                      />
           ));
