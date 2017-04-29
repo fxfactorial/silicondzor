362b855aa17e7d0698cd5a0ce47798ab4661e668
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-04-29 06:54:11 -0700

    Add Style component wrapper for Calendar

--- lib/event.jsx
+++ lib/event.jsx
@@ -19,27 +19,27 @@ export default class TechEvent extends Component {
   render() {
     return (
       <div>
-	<form>
-	  <p onClick={_ => this.props.close_modal()}> x </p>
-	  {this.props.prompt_msg(this.props.start, this.props.end)}
-	  <hr/>
-	  <div className={'modal-inputs'}>
-	    <label> Event title </label>
-	    <input type={'text'}
-		   value={this.state.event_title}
-		   onChange={e =>
-	      this.setState({...this.state, event_title:e.target.value})}/>
-	      <label> Event Description </label>
-	      <textarea type={'text'}
-			rows={8}
-			value={this.state.event_description}
-			onChange={e =>
-		this.setState({...this.state, event_description:e.target.value})}/>
-		<input type={'submit'}
-		       value={'Create Event'}
-		       onClick={this.submit_event}/>
-	  </div>
-	</form>
+	      <form>
+	        <p onClick={_ => this.props.close_modal()}> x </p>
+	        {this.props.prompt_msg(this.props.start, this.props.end)}
+	        <hr/>
+	        <div className={'modal-inputs'}>
+	          <label> Event title </label>
+	          <input type={'text'}
+		               value={this.state.event_title}
+		               onChange={e =>
+	            this.setState({...this.state, event_title:e.target.value})}/>
+	            <label> Event Description </label>
+	            <textarea type={'text'}
+			                  rows={8}
+			                  value={this.state.event_description}
+			                  onChange={e =>
+		            this.setState({...this.state, event_description:e.target.value})}/>
+		            <input type={'submit'}
+		                   value={'Create Event'}
+		                   onClick={this.submit_event}/>
+	        </div>
+	      </form>
       </div>
     );
   }
--- lib/tech-calendar.jsx
+++ lib/tech-calendar.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment_timezone from 'moment-timezone';
+import styled from 'styled-components';
+
+import { ContentWrapper } from './with-style';
+import colors from './colors';
 
 moment_timezone.tz.setDefault('Asia/Yerevan');
 BigCalendar.momentLocalizer(moment_timezone);
 
 const default_scroll_time = new Date(1970, 1, 1, 4);
+
+const CalendarWrapper = styled.div`
+  background-color: ${colors.site_colors.bg};
+  height: 600px;
+  box-shadow: 3px 3px 0px 0px ${colors.site_colors.banner};
+  font-weight: 300;
+  padding: 20px;
+  border-radius: 10px;
+`;
 
 export default class SDTechCalendar extends Component {
 
@@ -15,16 +28,18 @@ export default class SDTechCalendar extends Component {
 
   render () {
     return (
-      <div>
-        <BigCalendar
-          selectable
-          scrollToTime={default_scroll_time}
-          popup
-          onSelectEvent={this.event_selection_handler}
-          timeslots={1}
-          events={[]}
-          />
-      </div>
+      <ContentWrapper>
+        <CalendarWrapper>
+          <BigCalendar
+            selectable
+            scrollToTime={default_scroll_time}
+            popup
+            onSelectEvent={this.event_selection_handler}
+            timeslots={1}
+            events={[]}
+            />
+        </CalendarWrapper>
+      </ContentWrapper>
     );
   }
 }
